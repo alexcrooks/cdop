31c54b3eea0c43e62db350625c11268333ff9f2e
Alex Crooks <alex@alexcrooks.com>
2013-09-07 19:57:07 -0700

    remove extra closing td tag

--- include/legend.php
+++ include/legend.php
@@ -1,38 +1,37 @@
 <table id="legend">
 <tr>
 <td><b>1. Students are Doing</b>
-<table>
-<tr><td>L</td><td>Listening (to instructor/taking notes, etc.)</td></tr>
-<tr><td>Ind</td><td>Individual thinking/problem solving. Only mark when an instructor explicitly asks students to think about a clicker question or another question/problem on their own</td></tr>
-<tr><td>CG</td><td>Discuss clicker question in groups of 2 or more students</td></tr>
-<tr><td>WG</td><td>Working in groups on worksheet activity</td></tr>
-<tr><td>OG</td><td>Other assigned group activity (such as responding to instructor question)</td></tr>
-<tr><td>AnQ</td><td>Student answering a question posed by the instructor with rest of class listening</td></tr>
-<tr><td>SQ</td><td>Student asks question</td></tr>
-<tr><td>WC</td><td>Engaged in whole class discussion (by offering explanations, opinion, judgment, etc. to whole class)</td></tr>
-<tr><td>Prd</td><td>Making a prediction about upcoming activity (e.g. outcome of demo, experiment, etc.)</td></tr>
-<tr><td>SP</td><td>Presentation by student(s)</td></tr>
-<tr><td>TQ</td><td>Test or quiz</td></tr>
-<tr><td>W</td><td>Waiting (instructor late, working on fixing AV problems, instructor otherwise occupied, etc.)</td></tr>
-<tr><td>O</td><td>Other - explain in comments</td></tr>
-</table>
+    <table>
+    <tr><td>L</td><td>Listening (to instructor/taking notes, etc.)</td></tr>
+    <tr><td>Ind</td><td>Individual thinking/problem solving. Only mark when an instructor explicitly asks students to think about a clicker question or another question/problem on their own</td></tr>
+    <tr><td>CG</td><td>Discuss clicker question in groups of 2 or more students</td></tr>
+    <tr><td>WG</td><td>Working in groups on worksheet activity</td></tr>
+    <tr><td>OG</td><td>Other assigned group activity (such as responding to instructor question)</td></tr>
+    <tr><td>AnQ</td><td>Student answering a question posed by the instructor with rest of class listening</td></tr>
+    <tr><td>SQ</td><td>Student asks question</td></tr>
+    <tr><td>WC</td><td>Engaged in whole class discussion (by offering explanations, opinion, judgment, etc. to whole class)</td></tr>
+    <tr><td>Prd</td><td>Making a prediction about upcoming activity (e.g. outcome of demo, experiment, etc.)</td></tr>
+    <tr><td>SP</td><td>Presentation by student(s)</td></tr>
+    <tr><td>TQ</td><td>Test or quiz</td></tr>
+    <tr><td>W</td><td>Waiting (instructor late, working on fixing AV problems, instructor otherwise occupied, etc.)</td></tr>
+    <tr><td>O</td><td>Other - explain in comments</td></tr>
+    </table>
 </td>
 <td><b>2. Instructor is Doing</b>
-<table>
-<tr><td>Lec</td><td>Lecturing (presenting content, deriving mathematical results, presenting a problem solution)</td></tr>
-<tr><td>RtW</td><td>Real time writing (board, doc. projector, etc.) (often checked off with Lec)</td></tr>
-<tr><td>FUp</td><td>Follow-up/feedback on clicker question or activity to entire class</td></tr>
-<tr><td>PQ</td><td>Posing non-clicker question to students (non-rhetorical)</td></tr>
-<tr><td>CQ</td><td>Asking a clicker question (mark the entire time the instructor is using a clicker question, not just when first asked)</td></tr>
-<tr><td>AnQ</td><td>Listening to and answering student questions with entire class listening</td></tr>
-<tr><td>MG</td><td>Moving through class guiding ongoing student work during active learning task</td></tr>
-<tr><td>1o1</td><td>One-on-one: extended discussion with one or a few individuals neglecting the rest of the class (can be along with MG or AnQ)</td></tr>
-<tr><td>D/V</td><td>Showing or conducting demo, experiment, simulation, video, animation, or photo</td></tr>
-<tr><td>Adm</td><td>Administration (assign homework, return tests, etc)</td></tr>
-<tr><td>W</td><td>When there is an opportunity for an instructor to be interacting with or observing/listening to students or group activities and the instructor is not doing so.</td></tr>
-<tr><td>O</td><td>Other - explain in comments</td></tr>
-</table>
-</td>
+    <table>
+    <tr><td>Lec</td><td>Lecturing (presenting content, deriving mathematical results, presenting a problem solution)</td></tr>
+    <tr><td>RtW</td><td>Real time writing (board, doc. projector, etc.) (often checked off with Lec)</td></tr>
+    <tr><td>FUp</td><td>Follow-up/feedback on clicker question or activity to entire class</td></tr>
+    <tr><td>PQ</td><td>Posing non-clicker question to students (non-rhetorical)</td></tr>
+    <tr><td>CQ</td><td>Asking a clicker question (mark the entire time the instructor is using a clicker question, not just when first asked)</td></tr>
+    <tr><td>AnQ</td><td>Listening to and answering student questions with entire class listening</td></tr>
+    <tr><td>MG</td><td>Moving through class guiding ongoing student work during active learning task</td></tr>
+    <tr><td>1o1</td><td>One-on-one: extended discussion with one or a few individuals neglecting the rest of the class (can be along with MG or AnQ)</td></tr>
+    <tr><td>D/V</td><td>Showing or conducting demo, experiment, simulation, video, animation, or photo</td></tr>
+    <tr><td>Adm</td><td>Administration (assign homework, return tests, etc)</td></tr>
+    <tr><td>W</td><td>When there is an opportunity for an instructor to be interacting with or observing/listening to students or group activities and the instructor is not doing so.</td></tr>
+    <tr><td>O</td><td>Other - explain in comments</td></tr>
+    </table>
 </td>
 <td><b>3. Student Engagement</b>
 <table>
